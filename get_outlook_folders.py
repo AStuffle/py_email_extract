09a1c31430af1f25fecb1aa63833219a5d26bbbd
AStuffle <67909463+AStuffle@users.noreply.github.com>
2021-04-13 14:08:53 -0500

    Update get_outlook_folders.py
    Cleaned up and added notes.

--- get_outlook_folders.py
+++ get_outlook_folders.py
@@ -1,5 +1,5 @@
 # This script can be used to get folder object references from outlook.
-# Best to run it from comand line or shell.
+# Run it from cmd or shell.
 
 import win32com.client
 import os
@@ -13,7 +13,6 @@ def getfolders():
             print(i, name)
         except:
             pass
-
 def getsubfolders(parentfolder):
     for i in range(100):
         try:
@@ -23,6 +22,17 @@ def getsubfolders(parentfolder):
         except:
             pass
 
-print('Ready!')
-print('Use getfolders() to get a list of top-level folders and their index numbers.')
-print('Use getsubfolders(6) to get subfolders of folder 6 (Inbox)').
+print('\n\n')
+input('Press enter to list Outlook folders and their indexes...')
+print('\n\n')
+
+getfolders()
+print('\n\n')
+
+parentfolder = input('Select a folder number from above to see subfolders: ')
+print('\n\n')
+
+getsubfolders(parentfolder)
+print('\n\n')
+
+input('Press enter to exit.')
