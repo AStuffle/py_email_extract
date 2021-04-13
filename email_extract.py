2c923a78e20ee7c102a46a55fc36e78ea0f930de
AStuffle <67909463+AStuffle@users.noreply.github.com>
2021-04-13 14:05:05 -0500

    Update email_extract.py
    Added comments. Next up is to add multi-attachment support and a confirmation dialog before the download starts.

--- email_extract.py
+++ email_extract.py
@@ -1,10 +1,16 @@
 import win32com.client
 import os
+
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+
+# Target the folder you want to pull from. Use the get_outlook_folders script to get a complete list and re-target as needed.
 inbox = outlook.GetDefaultFolder(6).Folders[15]
+
+# 
 messages = inbox.Items
 message = messages.GetFirst()
 
+# Attachment save location. Needs to exist already.
 filepath = 'C:\\Users\\andrews.BROWN\\OneDrive - Brown Distributing Company, LTD\\NAA\\IRI Reports\\Exports'
 
 for m in messages:
@@ -12,5 +18,9 @@ for m in messages:
     attachments = message.Attachments
     attachment = attachments.Item(1)
     print (attachment)
+    
+    # Comment this out until you're sure you know what is going to get pulled!
+    # This will currently pull the first attachment from every email in the target folder!
     #attachment.SaveASFile(os.path.join(filepath,attachment.FileName))
+    
     message = messages.GetNext()
